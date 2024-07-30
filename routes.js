4fd4488cc81ad854059fb06d09c4a7fabb8165f8
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-07-30 16:48:12 +0700

    first commit

---
+++ routes.js
@@ -0,0 +1,72 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+module.exports = function (app, myDataBase) {
+   app.route('/').get((req, res) => {
+      res.render('index', {
+         title: 'Connected to Database',
+         message: 'Please login',
+         showLogin: true,
+         showRegistration: true,
+         showSocialAuth: true
+      });
+   });
+   
+   app.route('/login')
+      .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+         res.redirect('/profile');
+      });
+
+   app.route('/register')
+      .post((req, res, next) => {
+         myDataBase.findOne({ name: req.body.username }, function (err, user) {
+            if (err) return next(err);
+            if (user) return res.redirect('/');
+            const hash = bcrypt.hashSync(req.body.password, 12);
+            myDataBase.insertOne({
+               name: req.body.username,
+               password: hash
+            },
+               (err, doc) => {
+                  if (err) {
+                     console.log(err);
+                     return res.redirect('/')
+                  }
+
+                  next(null, doc.ops[0]);
+               }
+            )
+         });
+      }, passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+         res.redirect('/profile');
+      });
+
+
+   const ensureAuthenticated = (req, res, next) => {
+      if (req.isAuthenticated()) {
+         return next();
+      }
+      res.redirect('/');
+   };
+   app.route('/profile')
+      .get(ensureAuthenticated, (req, res) => {
+         res.render('profile', { username: req.user.name });
+      })
+   app.route('/logout')
+      .get((req, res) => {
+         req.logout();
+         res.redirect('/');
+      });
+   app.route('/auth/github')
+      .get(passport.authenticate('github'));
+   app.route('/auth/github/callback')
+      .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+         res.redirect('/profile');
+      });
+
+   app.use((req, res, next) => {
+      res.status(404)
+         .type('text')
+         .send('Not Found');
+   });
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+fccTesting(app); //For FCC testing purposes
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+myDB(async client => {
+  const myDataBase = await client.db('test').collection('people');
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
