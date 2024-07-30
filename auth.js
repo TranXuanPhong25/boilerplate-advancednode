3596a0831ee6966e8e19ce67de2b23e88c72acaf
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-07-30 17:00:33 +0700

    fix: update callbackurl

--- auth.js
+++ auth.js
@@ -29,7 +29,7 @@ module.exports = function (app, myDataBase) {
    passport.use(new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "https://localhost:8080/auth/github/callback"
+      callbackURL: process.env.GITHUB_CALLBACK_URL
    },
       function (accessToken, refreshToken, profile, cb) {
          console.log(profile);
